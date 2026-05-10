0506f4040658e2408daf464a61d4de26525e6a08
Махнатеев Степан <stepan01062004@mail.ru>
2026-05-10 22:08:45 +0300

    feat: add cmd files - Add bot-http cmd - Add bot-openai cmd

---
+++ cmd/bot-openai/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/PritOriginal/zero-agency-test/internal/app/bot"
+	"github.com/PritOriginal/zero-agency-test/internal/config"
+	slogger "github.com/PritOriginal/zero-agency-test/pkg/logger"
+)
+
+func main() {
+	cfg := config.MustLoad()
+	logger, err := slogger.SetupLogger(cfg.Env)
+	if err != nil {
+		log.Fatalf("error init logger: %v", err)
+	}
+
+	b := bot.NewWithOpenAiClient(logger, cfg)
+
+	go func() {
+		b.Run()
+	}()
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, syscall.SIGINT, syscall.SIGTERM)
+
+	<-done
+
+	b.Stop()
+}
